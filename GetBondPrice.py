79cc702e1c90640c96eeb379657dc49be0791b35
NiiAnnie <165346966+NiiAnnie@users.noreply.github.com>
2024-04-17 18:29:06 -0400

    Delete GetBondPrice.py

--- GetBondPrice.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-def getBondPrice(y, face, couponRate, m, ppy=1):
-    dfs = (1 + y)** -np.arange(1,m+1)
-    discounted_coupons = sum(face*couponRate*dfs)
-    discounted_face = face*dfs[-1]
-    return(discounted_coupons+discounted_face)
-
