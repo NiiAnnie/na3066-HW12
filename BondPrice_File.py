85085afa2657c963175537bd09fe30d399c8ee09
NiiAnnie <165346966+NiiAnnie@users.noreply.github.com>
2024-04-17 18:52:19 -0400

    Update BondPrice_File.py

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -1,8 +1,8 @@
 # No Loops!  All vectorized code.
 import numpy as np
 def getBondPrice(y, face, couponRate, m, ppy=1):
-    dfs = (1 + y)** -np.arange(1,m+1)
-    discounted_coupons = sum(face*couponRate*dfs)
+    dfs = (1 + y/ppy)** -np.arange(1,m*ppy+1)
+    discounted_coupons = sum(face*couponRate/ppy*dfs)
     discounted_face = face*dfs[-1]
     return(discounted_coupons+discounted_face)
 
